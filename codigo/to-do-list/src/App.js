b228018d38c117cbc8751cc490eb270339ca9f40
Rian Macedo <riansm100@gmail.com>
2023-06-22 15:38:51 -0300

    add components

--- codigo/to-do-list/src/App.js
+++ codigo/to-do-list/src/App.js
@@ -1,22 +1,70 @@
 import React, { useState } from 'react';
 import "./App.css";
 import Tasks from './Components/Tasks';
+import AddTask from './Components/AddTask';
+import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import TaskDetail from './Components/TaskDetail';
+
 
 function App() {
   const [tasks, setTasks] = useState([
     {
-      id:1,
+      id:uuidv4(),
       title:"Teste",
-      status:true
+      completed:true
+    },  {
+      id:uuidv4(),
+      title:"Teste",
+      completed:true
     }
   ])
 
+  const handleTaskAddition = (taskTitle) => {
+    console.log("TESTE", taskTitle)
+    setTasks((oldTasks)=>[...oldTasks, {id:uuidv4(), title:taskTitle, completed:false}])
+  }
+
+  const handleTaskClick = (idTask) => {
+    
+    setTasks (e => e.map(task => {
+
+      if (task.id === idTask){
+        return ({...task, completed: !task.completed})
+      } 
+        return ({...task})
+    })) 
+  } 
+
+  const handleRemoveTaskClick = (idTask) =>{
+    setTasks (e => e.filter(task => task.id !== idTask))
+  }
+
   return (
+    <Router>
     <div className="App">
       <div className="Container">
-      <Tasks tasks={tasks}/>
+      <h2 className='header'>Minhas Tarefas</h2>
+      <Route
+        path='/'
+        exact
+        render={()=>(
+          <>
+            <AddTask handleTaskAddition={handleTaskAddition}></AddTask> 
+            <Tasks handleRemoveTaskClick={handleRemoveTaskClick} handleTaskClick={handleTaskClick} tasks={tasks}/>
+          </>
+
+        )}
+      >
+      </Route>
+      <Route
+        path='/:taskTitle'
+        exact
+        component={TaskDetail}
+      ></Route>
       </div>
     </div>
+    </Router>
   );
 }
 
--- codigo/to-do-list/src/Components/Task/index.jsx
+++ codigo/to-do-list/src/Components/Task/index.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import "./styles.css"
+import {AiOutlineInfoCircle, AiOutlineCloseCircle} from "react-icons/ai";  
+import { useHistory } from 'react-router-dom';
 
-function Task({task}) {
+function Task({handleRemoveTaskClick, handleTaskClick,task}) {
+    const navigate = useHistory();
+
+    const handleTaskDetails = () =>{
+        navigate.push(`/${task.title}`)
+    }
 
     return (
     <>
-        <div className="container display-row">
-            <p className="title">{task.title}</p>
-            <div className="actions display-row">
-                <div className="icon-detalhe">ℹ</div>
-                <div className="icon-delete">❌</div>
+        <div 
+        className="container-task  display-row"
+        style={task.completed ? {borderLeft:"6px solid chartreuse"}: {}}
+        >
+            <div  className="title" onClick={() => handleTaskClick(task.id)}>
+                <p >{task.title}</p>
+            </div>
+            <div className="button-container display-row">
+                <button onClick={handleTaskDetails}className="button-display">
+                    <AiOutlineInfoCircle cursor={"pointer"} color='chartreuse' fontSize={25}/>
+                </button>
+                <button className="button-display" onClick={()=> handleRemoveTaskClick(task.id)}>
+                    <AiOutlineCloseCircle cursor={"pointer"}  color='chartreuse' fontSize={25}/>
+                </button>
             </div>
         </div>  
     </>
